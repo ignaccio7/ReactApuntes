2df3c1c1900194aa57ef399e33fde24700e13b12
ignaccio7 <nacho260298@gmail.com>
2024-05-14 17:56:22 -0400

    Prof playwright test

--- projects/17-pokeapi-cicd/tests/example.spec.js
+++ projects/17-pokeapi-cicd/tests/example.spec.js
@@ -14,14 +14,14 @@ test('App get all pokemons', async ({ page }) => {
 
   await expect(textContent?.length).toBeGreaterThan(0)
 })
-/*
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/)
 })
-
+/*
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/')
 
